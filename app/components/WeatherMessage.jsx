71315d62046389813d0a99e847f6ed105c6f8e7f
Timothy Kelly <kelltimo@oregonstate.edu>
2018-03-28 19:10:12 -0700

    Rename example page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,9 +4,7 @@ var WeatherMessage = ({temp, location}) => {
   return (<div>
     <h3>It's {temp}
     </h3>
-    <h3>
-      degrees in {location}</h3>
-
+    <h3> degrees in {location}</h3>
   </div>)
 };
 
