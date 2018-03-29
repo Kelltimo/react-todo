bf1e8cfcff21e7d4e92e84ef334cab261291af13
Timothy Kelly <kelltimo@oregonstate.edu>
2018-03-28 18:35:09 -0700

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 
 var Examples = (props) => {
-  return (<h3>Example Component</h3>)
+  return (<div>
+    <h3>Example Component</h3>
+    <p>Welcome to the examples page</p>
+  </div>)
 };
 
 module.exports = Examples;
