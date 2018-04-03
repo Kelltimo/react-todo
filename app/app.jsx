c870a780e10b3e0c9c8bfc1c019c7b030b85658a
Timothy Kelly <kelltimo@oregonstate.edu>
2018-04-02 19:36:09 -0700

    Switch to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -16,7 +16,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // Load CSS
-require('style!css!applicationStyle');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(<Router history={hashHistory}>
   <Route path="/" component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyle: 'app/styles/app.css'
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
